a624095efa969c10e042613e855df321a54305d1
SreekanthPatipati <psreekanth243@gmail.com>
2025-02-25 14:25:35 +0530

    Initial Commit

---
+++ DemoProject/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  DemoProject
+//
+//  Created by Sree on 25/02/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        
+        var name = "Sreekanth"
+        
+        
+    }
+
+
+}
+
